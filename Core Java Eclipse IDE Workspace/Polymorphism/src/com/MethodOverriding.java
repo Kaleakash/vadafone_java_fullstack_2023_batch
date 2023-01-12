12c25600f73dfb9a0b6a7678e793b650fbd104de
akash <akash300383@gmail.com>
2023-01-12 17:08:57 +0530

    run time polymorphism

--- Core Java Eclipse IDE Workspace/Polymorphism/src/com/MethodOverriding.java
+++ Core Java Eclipse IDE Workspace/Polymorphism/src/com/MethodOverriding.java
@@ -5,7 +5,7 @@ class Bike {
 	}
 }
 class Pulsar extends Bike {
-	@Override
+	@Override							// overriding the method 
 	void speed() {
 		System.out.println("90km/hr");
 	}
@@ -13,15 +13,26 @@ class Pulsar extends Bike {
 		System.out.println("Red");
 	}
 }
-class Honda extends Bike {
+class Honda extends Bike {					// re-usability the method 
 	void color() {
 		System.out.println("Gray");
 	}
 }
+class Tvs extends Bike {
+	void color() {
+		System.out.println("Black");
+	}
+	@Override							
+	void speed() {
+		super.speed();						// calling super class speed method
+		System.out.println("20km/hr");			// super class and sub class code merge together 
+	}
+}
 public class MethodOverriding {
 	public static void main(String[] args) {
-	Pulsar pu = new Pulsar();		 pu.color();   pu.speed();	
-	Honda hh = new Honda();	         hh.color();   hh.speed();
+	Pulsar pu = new Pulsar();		 pu.color();   pu.speed();		// 90km/hr
+	Honda hh = new Honda();	         hh.color();   hh.speed();		// 60km/hr
+	Tvs tv = new Tvs();				tv.color();   tv.speed();
 	}
 
 }
