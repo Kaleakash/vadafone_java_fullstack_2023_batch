90452955f32801afd773353a5278f423542761d9
akash <akash300383@gmail.com>
2023-01-16 18:26:05 +0530

    Exception handling program

---
+++ Core Java Eclipse IDE Workspace/ExceptionHandlingExamples/src/com/CheckedExceptionExample.java
@@ -0,0 +1,13 @@
+package com;
+
+public class CheckedExceptionExample {
+
+	public static void main(String[] args) throws Exception{
+	System.out.println("Hi");
+	//try {
+		Thread.sleep(3000);
+	//}catch(Exception e) {}
+	System.out.println("Now we stop, We will continue tomorow with Multithreading concept.");
+	}
+
+}
