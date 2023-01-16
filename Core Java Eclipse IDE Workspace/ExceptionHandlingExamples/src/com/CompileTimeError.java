4a646fe4ad256f3fa1e5fad4c36c5ef3333b73c8
akash <akash300383@gmail.com>
2023-01-16 15:56:12 +0530

    Access Specifiers Examples

---
+++ Core Java Eclipse IDE Workspace/ExceptionHandlingExamples/src/com/CompileTimeError.java
@@ -0,0 +1,10 @@
+package com;
+
+public class CompileTimeError {
+
+	public static void main(String[] args) {
+		int a;
+		System.out.println("Welcome to Java");
+	}
+
+}
---
+++ Core Java Eclipse IDE Workspace/ExceptionHandlingExamples/src/com/RuntimeException.java
@@ -0,0 +1,25 @@
+package com;
+
+public class RuntimeException {
+
+	public static void main(String[] args) {
+	System.out.println("Hi");
+	int a=10;
+	int b=1;
+	int abc[]= {10,20,30,40};
+			try {
+				int result1 = a/b;
+				System.out.println("Result is "+result1);
+				int result2 = abc[1]/10;
+				System.out.println("Result is "+result2);
+			}catch(Exception e) {
+				//System.out.println("I Take Care!");
+				//System.out.println(e.getMessage());  // short execption message 
+				System.out.println(e.toString()); // name of exception 
+			}
+	System.out.println("Bye....");
+	System.out.println("Bye....");
+	System.out.println("Bye....");
+	}
+
+}
