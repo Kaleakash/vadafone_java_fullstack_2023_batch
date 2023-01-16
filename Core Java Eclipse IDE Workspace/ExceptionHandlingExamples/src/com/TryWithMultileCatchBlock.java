98b0ff0242481eadd71110092a2ae90163202e55
akash <akash300383@gmail.com>
2023-01-16 16:22:34 +0530

    Exception

---
+++ Core Java Eclipse IDE Workspace/ExceptionHandlingExamples/src/com/TryWithMultileCatchBlock.java
@@ -0,0 +1,25 @@
+package com;
+
+public class TryWithMultileCatchBlock {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		int num[]= {0,20,30,40};
+		String abc="10a";
+		try {
+			int res =100/num[2];			// num[0], num[5], num[2]
+			System.out.println("res "+res);
+			System.out.println(abc+10);
+				System.out.println(Integer.parseInt(abc)+10);
+			System.out.println("No Exception");
+		}catch (ArithmeticException e) {
+			System.out.println("Divided by Zero "+e.toString());
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array Index "+e.toString());
+		}catch(Exception e) {
+			System.out.println("Generic "+e.toString());
+		}
+		System.out.println("finish");
+	}
+
+}
