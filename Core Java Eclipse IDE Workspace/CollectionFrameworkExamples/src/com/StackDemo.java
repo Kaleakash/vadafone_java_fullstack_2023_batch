61567f1d8ae6fe0f893778e2dc07f9507b4b1aff
Akash <akash300383@gmail.com>
2023-01-19 17:01:44 +0530

    done

--- Core Java Eclipse IDE Workspace/CollectionFrameworkExamples/src/com/StackDemo.java
+++ Core Java Eclipse IDE Workspace/CollectionFrameworkExamples/src/com/StackDemo.java
@@ -7,7 +7,7 @@ public class StackDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Stack ss = new Stack();
-		ss.push(10);
+		ss.push(10);			// to add 
 		ss.push(20);
 		ss.push(30);
 		ss.push(40);
@@ -15,10 +15,11 @@ public class StackDemo {
 		System.out.println(ss);
 		System.out.println(ss.pop());	// remove top most element 
 		System.out.println(ss);
-		System.out.println(ss.peek());  // it check only top most element 
+		System.out.println(ss.peek());  // it check only top most element but not remove 
 		System.out.println(ss);
 		System.out.println(ss.search(40));   // position of element count from top from 1
 		System.out.println(ss.search(400)); // if not present return -1
+		System.out.println(ss.size());
  	}
 
 }
