fb59c11ca0ed8611aa546c07fa3288c9fd89ef96
Akash <akash300383@gmail.com>
2023-01-20 16:28:33 +0530

    done

---
+++ Core Java Eclipse IDE Workspace/CollectionFrameworkExamples/src/com/CollectionsUtilityClass.java
@@ -0,0 +1,31 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsUtilityClass {
+
+	public static void main(String[] args) {
+		List<String> listOfStd = new ArrayList<>();
+		listOfStd.add("Lex"); listOfStd.add("Steven"); listOfStd.add("John"); listOfStd.add("Raj"); listOfStd.add("Leena");
+		listOfStd.add("Ajay");
+		System.out.println("Before sort");
+		for(String name:listOfStd) {
+			System.out.print(name+" ");
+		}
+		Collections.sort(listOfStd);
+			System.out.println();
+		System.out.println("After sort - Asc");
+		for(String name:listOfStd) {
+			System.out.print(name+" ");
+		}
+		Collections.reverse(listOfStd);
+			System.out.println();
+		System.out.println("After sort - Desc");
+		for(String name:listOfStd) {
+			System.out.print(name+" ");
+		}
+	}
+
+}
