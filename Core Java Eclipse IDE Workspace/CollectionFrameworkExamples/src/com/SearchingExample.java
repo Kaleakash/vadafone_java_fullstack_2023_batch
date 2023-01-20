b838e31e5761fbf2b181c350407818bc0a6bca1c
Akash <akash300383@gmail.com>
2023-01-20 18:02:00 +0530

    done

--- Core Java Eclipse IDE Workspace/CollectionFrameworkExamples/src/com/SearchingExample.java
+++ Core Java Eclipse IDE Workspace/CollectionFrameworkExamples/src/com/SearchingExample.java
@@ -1,5 +1,6 @@
 package com;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SearchingExample {
@@ -12,17 +13,37 @@ public class SearchingExample {
 		}
 		return -1;
 	}
+	public static void binarySearch(int num[], int first, int last, int key) {
+		int mid = (first+last)/2;			// mid =0+6, mid = 3 key = 5 num[3]
+		while(first <= last) {
+			if(num[mid]<key) {
+				first  = mid+1;
+			}else if(num[mid]==key) {
+				System.out.println("Element present");
+				break;
+			}else {
+				last = mid-1;
+			}
+			mid = (first+last)/2;
+		}
+		if(first>last) {
+			System.out.println("Element not present");
+		}
+	}
 	public static void main(String[] args) {
 		int num[]= {1,5,2,4,9,8,7};
 		System.out.println("Plz enter the element to search from array");
 		Scanner sc = new Scanner(System.in);
 		int key = sc.nextInt();
-		int result = linearSearch(num, key);
-		if(result==1) {
-			System.out.println("Element  present");
-		}else {
-			System.out.println("Element not present");
-		}
+		Arrays.sort(num);	// 1,2,4,5,7,8,9		 pre defined sorting technique 
+		binarySearch(num, 0, num.length-1, key);
+		
+//		int result = linearSearch(num, key);
+//		if(result==1) {
+//			System.out.println("Element  present");
+//		}else {
+//			System.out.println("Element not present");
+//		}
 	}
 
 }
