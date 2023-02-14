6c4a76094fbcf6a6d8944047810334cbc8703367
Akash <akash300383@gmail.com>
2023-02-14 17:49:27 +0530

    done

--- Java Phase 2 Programs/JDBC_CRUD_Operation/src/com/service/EmployeeService.java
+++ Java Phase 2 Programs/JDBC_CRUD_Operation/src/com/service/EmployeeService.java
@@ -1,5 +1,24 @@
 package com.service;
 
+import com.bean.Employee;
+import com.dao.EmployeeDao;
+
 public class EmployeeService {
 
+	EmployeeDao ed = new EmployeeDao();
+	
+	public String storeEmployee(Employee emp) {
+		
+		if(emp.getSalary()>12000) {
+				
+				if(ed.storeEmployee(emp)>0) {
+					return "Record insered in database";
+				}else {
+					return "Record didn't insert";
+				}
+				
+		}else {
+			return "Salary must be >12000";
+		}
+	}
 }
