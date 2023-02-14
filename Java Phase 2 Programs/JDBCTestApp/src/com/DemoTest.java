5850d8754526985361cce799bee54482d76d6559
Akash <akash300383@gmail.com>
2023-02-14 15:25:04 +0530

    done

--- Java Phase 2 Programs/JDBCTestApp/src/com/DemoTest.java
+++ Java Phase 2 Programs/JDBCTestApp/src/com/DemoTest.java
@@ -15,25 +15,52 @@ public class DemoTest {
 //		System.out.println("id is "+rs.getInt(1)+" Name is "+rs.getString(2)+" Salary is "+rs.getFloat(3));
 //	}
 			
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root@123");
-		Statement stmt = con.createStatement();
-		
-		
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root@123");
+	Statement stmt = con.createStatement();
+	
 //		System.out.println("Retrieve all employee records one by one");
 //		ResultSet rs = stmt.executeQuery("select * from employee");
 //		while(rs.next()) {
 //			System.out.println("id is "+rs.getInt(1)+" Name is "+rs.getString(2)+" Salary is "+rs.getFloat(3));
 //		}
+//		System.out.println("Retrieve employee record with condition ");
+//		//ResultSet rs = stmt.executeQuery("select * from employee where salary > 30000");
+//		//ResultSet rs = stmt.executeQuery("select * from employee where name ='Raj' ");
+//		ResultSet rs = stmt.executeQuery("select * from employee where id in(4,5) ");
+//		while(rs.next()) {
+//			System.out.println("id is "+rs.getInt(1)+" Name is "+rs.getString(2)+" Salary is "+rs.getFloat(3));
+//		}
 		
-		System.out.println("Retrieve employee record with condition ");
-		//ResultSet rs = stmt.executeQuery("select * from employee where salary > 30000");
-		//ResultSet rs = stmt.executeQuery("select * from employee where name ='Raj' ");
-		ResultSet rs = stmt.executeQuery("select * from employee where id in(4,5) ");
-		while(rs.next()) {
-			System.out.println("id is "+rs.getInt(1)+" Name is "+rs.getString(2)+" Salary is "+rs.getFloat(3));
-		}
-			
+//		System.out.println("Insert Query");
+//		
+//		int temp = stmt.executeUpdate("insert into employee values(7,'Neena',18000)");
+//		if(temp>0) {
+//			System.out.println("record inserted successfully");
+//		}
+		
+	
+//		System.out.println("delete Query");
+//	
+//		int temp = stmt.executeUpdate("delete from employee where salary > 30000");
+//		System.out.println(" number of records deleted "+temp);
+//		if(temp>0) {
+//			System.out.println("record deleted successfully");
+//		}else {
+//			System.out.println("Record not present");
+//		}
+		
+	
+	System.out.println("update Query");
+	
+	int temp = stmt.executeUpdate("update employee set salary = 28000 where id=1");
+	System.out.println(" number of records updated "+temp);
+	if(temp>0) {
+		System.out.println("record updated successfully");
+	}else {
+		System.out.println("Record not present");
+	}
+	
 		} catch (Exception e) {
 			System.out.println(e);
 		}
