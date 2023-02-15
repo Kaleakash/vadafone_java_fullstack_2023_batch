84ca5624cb9f17f3d9581078aeaf5311d63dcfd9
Akash <akash300383@gmail.com>
2023-02-15 18:29:05 +0530

    done

---
+++ Java Phase 2 Programs/HibernateTest/src/main/java/com/Employee.java
@@ -0,0 +1,37 @@
+package com;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")					
+public class Employee {
+@Id							// column with pk (primary key)
+@Column(name="id")
+private int id;
+@Column(name="name")				// if column name is empname
+private String name;
+@Column(name="salary")
+private float salary;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public float getSalary() {
+	return salary;
+}
+public void setSalary(float salary) {
+	this.salary = salary;
+}
+
+}
