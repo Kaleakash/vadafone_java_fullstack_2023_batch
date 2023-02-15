b3d11a2297c512d945e0d26980fe4507b05ff56c
Akash <akash300383@gmail.com>
2023-02-15 14:46:57 +0530

    done

--- Java Phase 2 Programs/JDBC_CRUD_Operation/src/com/main/DemoTest.java
+++ Java Phase 2 Programs/JDBC_CRUD_Operation/src/com/main/DemoTest.java
@@ -1,22 +1,47 @@
 package com.main;
 
+import java.util.Scanner;
+
 import com.bean.Employee;
 import com.service.EmployeeService;
 
 public class DemoTest {
 
-	public static void main(String[] args) {
-		Employee emp = new Employee();
-	emp.setId(10);
-	emp.setName("John");
-	emp.setSalary(10000);
-	
-	// emp, which hold one employee record which map to database table record 
-	
-	EmployeeService es = new EmployeeService();
-	String result = es.storeEmployee(emp);
-	System.out.println(result);
-
+	public static void main(String[] args) {//	EmployeeService es = new EmployeeService();
+		Scanner sc = new Scanner(System.in);
+		EmployeeService es = new EmployeeService();
+		int choice;
+		int id;
+		String name;
+		float salary;
+		String result;
+		String con;
+		do {
+			System.out.println("1:Add Employee");
+			System.out.println("Plz enter your choice");
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1:System.out.println("Add Employee details");
+				  System.out.println("Enter the id");
+				  id = sc.nextInt();
+				  System.out.println("Enter the name");
+				  name = sc.next();
+				  System.out.println("Enter the salary");
+				  salary = sc.nextFloat();
+				  Employee emp = new Employee();
+				  emp.setId(id);
+				  emp.setName(name);
+				  emp.setSalary(salary);
+				  result = es.storeEmployee(emp);
+				  System.out.println(result);
+				  break;
+			default:System.out.println("Wrong choice");
+			     break;
+			}
+			System.out.println("Do you want to continue?(yes/no)");
+		con = sc.next();
+		} while (con.equalsIgnoreCase("yes"));
+		System.out.println("Thank you!");
 	}
 
 }
