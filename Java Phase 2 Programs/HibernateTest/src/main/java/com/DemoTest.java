8bbed3746fadcf3193fe0cc97cc40a78852ea9f7
Akash <akash300383@gmail.com>
2023-02-16 16:15:44 +0530

    done

--- Java Phase 2 Programs/HibernateTest/src/main/java/com/DemoTest.java
+++ Java Phase 2 Programs/HibernateTest/src/main/java/com/DemoTest.java
@@ -1,31 +1,54 @@
 package com;
-
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.Transaction;
+//import org.hibernate.cfg.Configuration;
+import org.hibernate.*;
+import org.hibernate.cfg.*;
 public class DemoTest {
-
 	public static void main(String[] args) {
-		
 		Configuration con = new Configuration();
 		con.configure("hibernate.cfg.xml");
 		System.out.println("File loaded successfully");
 		SessionFactory sf = con.buildSessionFactory();  // It is equal to Connection in JDBC.
 		Session session  = sf.openSession();			// it is equal to Statement or PreparedStatement 
 		
-		Employee emp = new Employee();
-		emp.setId(10);
-		emp.setName("Lex");
-		emp.setSalary(28000);
-		
 		Transaction tran = session.getTransaction();
+		
+//		System.out.println("Insert Record");
+//		Employee emp = new Employee();
+//		emp.setId(10);
+//		emp.setName("Lex");
+//		emp.setSalary(28000);
+//		tran.begin();
+//		session.save(emp);						// insert into employee values(10,'lex',28000);
+//		tran.commit();
+//		System.out.println("Record inseted successfully..");
+	
+//		System.out.println("Delete Query");
+//		Employee emp = session.get(Employee.class, 1);
+//		if(emp==null) {
+//			System.out.println("Record not present");
+//		}else {
+//			tran.begin();
+//			session.delete(emp);			// delete from employee where id = 1;
+//			tran.commit();
+//			System.out.println("Record deleted successfully");
+//		}
+		
+		System.out.println("update Query");
+		Employee emp = session.get(Employee.class, 2);
+		if(emp==null) {
+			System.out.println("Record not present");
+		}else {
 			tran.begin();
-			session.save(emp);						// insert into employee values(10,'lex',28000);
+					emp.setSalary(34000);
+					session.update(emp);			// update employee set salary = 34000 where id=1
 			tran.commit();
-			System.out.println("Record inseted successfully..");
+			System.out.println("Record updated successfully");
+		}
+		
 	}
 
 }
+
