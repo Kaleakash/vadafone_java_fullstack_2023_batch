8ce0a62e6169dc79526663970d53aad19b3eb0b0
Akash <akash300383@gmail.com>
2023-02-17 15:24:43 +0530

    done

--- Java Phase 2 Programs/ProductManagementSystemUsingHibernate/src/main/java/com/main/App.java
+++ Java Phase 2 Programs/ProductManagementSystemUsingHibernate/src/main/java/com/main/App.java
@@ -1,9 +1,17 @@
 package com.main;
 
+import com.entity.Product;
+import com.service.ProductService;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ProductService ps = new ProductService();
+		Product p = new Product();
+		p.setPname("Computer");
+		p.setPrice(45000);
+		String result = ps.storeProduct(p);
+		System.out.println(result);
 
 	}
 
