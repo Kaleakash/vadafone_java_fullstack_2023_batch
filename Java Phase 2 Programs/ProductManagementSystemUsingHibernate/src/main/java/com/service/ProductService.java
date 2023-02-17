d54d4a76acc25318d3422966d1259e8e70deb2cd
Akash <akash300383@gmail.com>
2023-02-17 15:12:32 +0530

    done

--- Java Phase 2 Programs/ProductManagementSystemUsingHibernate/src/main/java/com/service/ProductService.java
+++ Java Phase 2 Programs/ProductManagementSystemUsingHibernate/src/main/java/com/service/ProductService.java
@@ -1,5 +1,20 @@
 package com.service;
 
+import com.dao.ProductDao;
+import com.entity.Product;
+
 public class ProductService {
 
+	ProductDao pd = new ProductDao();
+	public String storeProduct(Product product) {
+		if(product.getPrice()<500) {
+			return "Product price must > 500";
+		}else {
+			if(pd.storeProduct(product)>0) {
+				return "Product stored successfully";
+			}else {
+				return "Product didn't store";
+			}
+		}
+	}
 }
