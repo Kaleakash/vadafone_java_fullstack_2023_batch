974933e4642a5b821db3bdd052642ce5e220756c
Akash <akash300383@gmail.com>
2023-02-21 15:04:25 +0530

    done

---
+++ Java Phase 2 Programs/SimpleServletApp/src/main/java/com/Home.java
@@ -0,0 +1,45 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw = response.getWriter();
+	pw.println("Welcome to Home page using get method");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		PrintWriter pw = response.getWriter();
+		pw.println("Welcome to Home page using post method");
+	}
+
+}
--- Java Phase 2 Programs/SimpleServletApp/src/main/java/com/LoginServlet.java
+++ Java Phase 2 Programs/SimpleServletApp/src/main/java/com/LoginServlet.java
@@ -3,6 +3,7 @@ package com;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,49 +15,41 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public LoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter pw = response.getWriter();
-		
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+		response.setContentType("text/html");
 		String emailid = request.getParameter("emailid");
-		
 		String password = request.getParameter("pass");
-		
 		if(emailid.equals("akash@gmail.com") && password.equals("123")) {
-			pw.println("Successfully login with get method");
+	pw.println("Successfully login with get method");
+			rd1.forward(request, response);
 		}else {
 			pw.println("failure try once again with get method");
-		}
-		
+			rd2.include(request, response);
+		}		
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//doGet(request, response);
-PrintWriter pw = response.getWriter();
+		PrintWriter pw = response.getWriter();
 		
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+		response.setContentType("text/html");
 		String emailid = request.getParameter("emailid");
-		
 		String password = request.getParameter("pass");
-		
 		if(emailid.equals("akash@gmail.com") && password.equals("123")) {
 			pw.println("Successfully login with post method");
+			rd1.forward(request, response);
 		}else {
 			pw.println("failure try once again with post method");
+			rd2.include(request, response);
 		}
 	}
 
