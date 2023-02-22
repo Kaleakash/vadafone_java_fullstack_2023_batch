0ed09807dadad3d5b53d636a6f97737abd0e4ab6
Akash <akash300383@gmail.com>
2023-02-22 15:49:22 +0530

    done

--- Java Phase 2 Programs/MVCApp/src/main/java/com/dao/ProductDao.java
+++ Java Phase 2 Programs/MVCApp/src/main/java/com/dao/ProductDao.java
@@ -1,5 +1,35 @@
 package com.dao;
 
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.entity.Product;
+import com.resource.DbResource;
+
 public class ProductDao {
 
+	public int storeProduct(Product product) {
+		SessionFactory sf = DbResource.getSessionFactory();
+		Session session = sf.getCurrentSession();
+		Transaction tran = session.getTransaction();
+		tran.begin();
+			session.save(product);
+		tran.commit();
+		
+		return 1;
+	}
+	
+	public List<Product> findAllProduct() {
+		SessionFactory sf = DbResource.getSessionFactory();
+		Session session = sf.getCurrentSession();
+		TypedQuery tq = session.createQuery("from Product");
+		List<Product> listOfProdut = tq.getResultList();
+		return listOfProdut;
+	}
+	
 }
