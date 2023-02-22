10aeec6debd3161f98f56cec3e4f0a71c7ed3e97
Akash <akash300383@gmail.com>
2023-02-22 16:06:55 +0530

    done

--- Java Phase 2 Programs/MVCApp/src/main/java/com/entity/Product.java
+++ Java Phase 2 Programs/MVCApp/src/main/java/com/entity/Product.java
@@ -1,6 +1,16 @@
 package com.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="productdetails")
 public class Product {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private int pid;
 private String pname;
 private float price;
