6e4076bb355740845822d684302a835b8544e844
Akash <akash300383@gmail.com>
2023-02-22 16:19:40 +0530

    done

--- Java Phase 2 Programs/MVCApp/src/main/java/com/controller/ProductController.java
+++ Java Phase 2 Programs/MVCApp/src/main/java/com/controller/ProductController.java
@@ -1,11 +1,17 @@
 package com.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.entity.Product;
+import com.service.ProductService;
 
 /**
  * Servlet implementation class ProductController
@@ -22,20 +28,33 @@ public class ProductController extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	// retrieve product details 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	// store product details 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		PrintWriter pw = response.getWriter();
+		String pname = request.getParameter("pname");
+		float price = Float.parseFloat(request.getParameter("price"));
+		int quantity = Integer.parseInt(request.getParameter("quantity"));
+		String imageUrl = request.getParameter("imageUrl");
+		
+		Product p = new Product();
+		p.setPname(pname);
+		p.setPrice(price);
+		p.setQuantity(quantity);
+		p.setImageUrl(imageUrl);
+		
+		ProductService ps = new ProductService();
+		String result  = ps.storeProduct(p);
+		pw.println(result);
+		RequestDispatcher rd = request.getRequestDispatcher("storeProduct.jsp");
+		rd.include(request, response);
+		
+		
 	}
 
 }
