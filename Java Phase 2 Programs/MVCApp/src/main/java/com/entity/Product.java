a65d1bde116131086329a0c90b6ff7ec4f79f8f9
Akash <akash300383@gmail.com>
2023-02-22 16:04:20 +0530

    done

--- Java Phase 2 Programs/MVCApp/src/main/java/com/entity/Product.java
+++ Java Phase 2 Programs/MVCApp/src/main/java/com/entity/Product.java
@@ -1,5 +1,45 @@
 package com.entity;
 
 public class Product {
+private int pid;
+private String pname;
+private float price;
+private int quantity;
+private String imageUrl;
+public int getPid() {
+	return pid;
+}
+public void setPid(int pid) {
+	this.pid = pid;
+}
+public String getPname() {
+	return pname;
+}
+public void setPname(String pname) {
+	this.pname = pname;
+}
+public float getPrice() {
+	return price;
+}
+public void setPrice(float price) {
+	this.price = price;
+}
+public int getQuantity() {
+	return quantity;
+}
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}
+public String getImageUrl() {
+	return imageUrl;
+}
+public void setImageUrl(String imageUrl) {
+	this.imageUrl = imageUrl;
+}
+@Override
+public String toString() {
+	return "Product [pid=" + pid + ", pname=" + pname + ", price=" + price + ", quantity=" + quantity + ", imageUrl="
+			+ imageUrl + "]";
+}
 
 }
