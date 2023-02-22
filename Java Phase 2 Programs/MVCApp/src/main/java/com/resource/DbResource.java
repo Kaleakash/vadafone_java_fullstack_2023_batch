78b949ee8110dd8785e875245276b5fca1fc5de9
Akash <akash300383@gmail.com>
2023-02-22 15:44:10 +0530

    done

--- Java Phase 2 Programs/MVCApp/src/main/java/com/resource/DbResource.java
+++ Java Phase 2 Programs/MVCApp/src/main/java/com/resource/DbResource.java
@@ -1,5 +1,14 @@
 package com.resource;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class DbResource {
 
+	
+	public static SessionFactory getSessionFactory() {
+		Configuration con = new Configuration();
+		con.configure("hibernate.cfg.xml");
+		return con.buildSessionFactory();
+	}
 }
