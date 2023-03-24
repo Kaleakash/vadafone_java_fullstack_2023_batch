9bb0a59f8feb035fcdfb7ae57174b67a4730bb35
Akash <akash300383@gmail.com>
2023-03-24 16:20:08 +0530

    done

---
+++ Spring boot programs/SimpleSpringBootMVC/src/main/java/com/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyController {
+
+	@RequestMapping(value = "hello",method = RequestMethod.GET)
+	public String openDemoPage() {
+		// coding...
+		return "demo";
+	}
+}
--- Spring boot programs/SimpleSpringBootMVC/src/main/java/com/main/DemoTest.java
+++ Spring boot programs/SimpleSpringBootMVC/src/main/java/com/main/DemoTest.java
@@ -3,7 +3,7 @@ package com.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
 public class DemoTest {
 
 	public static void main(String[] args) {
