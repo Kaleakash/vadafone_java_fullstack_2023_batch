aa50f58f057799910b629925615598708ac65813
Akash <akash300383@gmail.com>
2023-03-24 15:42:40 +0530

    done

---
+++ Spring boot programs/SimpleSpringBootMVC/src/main/java/com/main/DemoTest.java
@@ -0,0 +1,14 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTest {
+
+	public static void main(String[] args) {
+	SpringApplication.run(DemoTest.class, args);
+	System.out.println("Spring boot up");
+	}
+
+}
