d796729a6f211c7ff3ba8537ac5ed1af807e2284
Akash <akash300383@gmail.com>
2023-03-27 16:15:07 +0530

    done

--- Spring boot programs/SpringBootWithThymeleafDemo/SpringBootWithThymeleafDemo/src/main/java/com/controller/EmployeeController.java
+++ Spring boot programs/SpringBootWithThymeleafDemo/SpringBootWithThymeleafDemo/src/main/java/com/controller/EmployeeController.java
@@ -5,14 +5,20 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bean.Employee;
 
 @Controller
 public class EmployeeController {
-
 	
+	
+	//http://localhost:8080/addEmployee
+
+	@RequestMapping(value = "addEmployee",method = RequestMethod.GET)
 	public String displayEmployeeInfo(Model mm) {
+		
 		List<Employee> al = new ArrayList<>();
 		al.add(new Employee(1, "Ravi", 12000));
 		al.add(new Employee(2, "Ramesh", 14000));
