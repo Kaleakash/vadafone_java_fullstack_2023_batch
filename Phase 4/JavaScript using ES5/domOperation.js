7af2348074d6eb989cfbd64ada0939b7d559a961
Akash <akash300383@gmail.com>
2023-05-18 15:09:20 +0530

    done

--- Phase 4/JavaScript using ES5/domOperation.js
+++ Phase 4/JavaScript using ES5/domOperation.js
@@ -1,3 +1,25 @@
 function addData() {
-    alert("Event fired...")
+    //alert("Event fired...")
+    // creating dynamic tag 
+    var name = document.getElementById("uname").value;
+    var age = document.getElementById("age").value;
+    
+    var pTag = document.createElement("p");
+    // creating value for that tag 
+    //var pTagValue = document.createTextNode("Welcome to JavaScript Training");
+
+    var pTagValue = document.createTextNode("Name is "+name+" Age is "+age);
+    // we add contents inside that tag 
+    pTag.appendChild(pTagValue);   //<p>Welcome to JavaScript Training</p>
+    if(age>30){
+        pTag.setAttribute("class","myFirstClass");
+    }else {
+        pTag.setAttribute("class","mySecondClass");
+    }
+
+
+    // get reference of div tag using id attribute 
+    document.getElementById("result").appendChild(pTag);
+    document.getElementById("uname").value="";
+    document.getElementById("age").value="";
 }
