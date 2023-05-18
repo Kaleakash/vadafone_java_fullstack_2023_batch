1c965384717219bd0ee968bf2fb16562cd5dad3f
Akash <akash300383@gmail.com>
2023-05-18 16:08:41 +0530

    done

---
+++ Phase 4/JavaScript Using ES6/varletandconst.js
@@ -0,0 +1,23 @@
+var a=10;
+a=20;
+var a=30;
+document.write("value of a "+a);
+let b=10;
+b="Ravi";
+//let b=30;     error 
+document.write("Value of b "+b);
+for(var i=0;i<100;i++){
+
+}
+document.write("Value of i is "+i)
+
+for(let j=0;j<100;j++){
+
+}
+//document.write("Value of j is "+j); // we can't access outside loop 
+var x=100;
+x=200;
+let y=100;
+y=200;
+const z=100;
+//z=300;          // can't change 
