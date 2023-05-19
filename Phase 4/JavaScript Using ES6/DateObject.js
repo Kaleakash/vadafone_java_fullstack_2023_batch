b42e04152fddab788a365f2592e0900a52d522a0
Akash <akash300383@gmail.com>
2023-05-19 15:25:08 +0530

    done

--- Phase 4/JavaScript Using ES6/DateObject.js
+++ Phase 4/JavaScript Using ES6/DateObject.js
@@ -1,9 +1,10 @@
 let dd = new Date();
-document.write("<br/> "+dd);
-document.write("<br/> "+dd.getDate())
-dd.setMonth(dd.getMonth()+1);
-document.write("<br/> "+dd.getMonth())
-document.write("<br/> "+dd.getFullYear())
-document.write("<br/> "+dd.getHours())
-document.write("<br/> "+dd.getMinutes())
-document.write("<br/> "+dd.getSeconds())
+//document.write(dd.getTi)
+// document.write("<br/> "+dd);
+// document.write("<br/> "+dd.getDate())
+// dd.setMonth(dd.getMonth()+1);
+// document.write("<br/> "+dd.getMonth())
+// document.write("<br/> "+dd.getFullYear())
+// document.write("<br/> "+dd.getHours())
+// document.write("<br/> "+dd.getMinutes())
+// document.write("<br/> "+dd.getSeconds())
---
+++ Phase 4/JavaScript Using ES6/MathObject.js
@@ -0,0 +1,5 @@
+document.write("<br/> "+Math.PI)
+document.write("<br/> "+Math.E)
+document.write("<br/> "+Math.sqrt(9))
+document.write("<br/> "+Math.max(100,50));
+document.write("<br/> "+Math.min(100,50));
