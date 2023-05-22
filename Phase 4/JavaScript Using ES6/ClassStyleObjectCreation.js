e82b7600f1f3c379f6ee9b7855b9083ff7785aa0
Akash <akash300383@gmail.com>
2023-05-22 14:40:38 +0530

    done

--- Phase 4/JavaScript Using ES6/ClassStyleObjectCreation.js
+++ Phase 4/JavaScript Using ES6/ClassStyleObjectCreation.js
@@ -31,15 +31,32 @@ emp1.display();*/
 // emp2.display();
 
 // ES6 employee object with constructor 
-class Employee {
-    constructor() {
-        document.write("<br> Employee object created...")
-    }
-    display() {
-        document.write("<br> Employee object function")
-    }
-}
-let emp1 = new Employee();
+// class Employee {
+//     constructor() {
+//         document.write("<br> Employee object created...")
+//     }
+//     display() {
+//         document.write("<br> Employee object function")
+//     }
+// }
+// let emp1 = new Employee();   // empty constructor 
 //emp1.display();
 
-
+// In ES6 creating more than one object using parameter constructor 
+class Employee {
+    constructor(id,name,age){
+        this.id = id;
+        this.name = name;
+        this.age = age;        
+    }
+    display() {
+        document.write("<br> Emloyee details");
+        document.write("<br> id is "+this.id);
+        document.write("<br> name is "+this.name);
+        document.write("<br> age is "+this.age);
+    }
+}
+let emp1 = new Employee(100,"Steven",21);   // parameter construtor 
+let emp2 = new Employee(101,"Ajay",25);
+emp1.display();
+emp2.display();
