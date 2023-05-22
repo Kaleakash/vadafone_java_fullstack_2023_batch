7c0928a678d713b78d13b38a3c36fa2fcd7f37a7
Akash <akash300383@gmail.com>
2023-05-22 14:29:53 +0530

    done

--- Phase 4/JavaScript Using ES6/ClassStyleObjectCreation.js
+++ Phase 4/JavaScript Using ES6/ClassStyleObjectCreation.js
@@ -14,17 +14,32 @@ function Employee() {
 var emp1 = new Employee();
 emp1.display();*/
 //ES6 style 
+// class Employee {
+//     id=100;
+//     name = "Ajay";
+//     age = 21;
+//     display() {
+//         document.write("<br> Employee object function")
+//         document.write("<br> Id is "+this.id);
+//         document.write("<br> Name is "+this.name);
+//         document.write("<br> Age is "+this.age);
+//     }
+// }
+// let emp1 = new Employee();
+// emp1.display();
+// let emp2 = new Employee();
+// emp2.display();
+
+// ES6 employee object with constructor 
 class Employee {
-    id=100;
-    name = "Ajay";
-    age = 21;
+    constructor() {
+        document.write("<br> Employee object created...")
+    }
     display() {
         document.write("<br> Employee object function")
-        document.write("<br> Id is "+this.id);
-        document.write("<br> Name is "+this.name);
-        document.write("<br> Age is "+this.age);
     }
 }
 let emp1 = new Employee();
-emp1.display();
+//emp1.display();
+
 
