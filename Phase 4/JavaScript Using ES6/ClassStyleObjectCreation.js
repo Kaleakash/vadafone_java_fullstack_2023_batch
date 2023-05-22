b02b0ed1d496b918d54db51ccc8d31bad9b3fbc0
Akash <akash300383@gmail.com>
2023-05-22 14:17:53 +0530

    done

---
+++ Phase 4/JavaScript Using ES6/ClassStyleObjectCreation.js
@@ -0,0 +1,30 @@
+/*
+ES5 style 
+function Employee() {
+        this.id =100;
+        this.name = "Ajay"
+        this.age  =21;
+        thid.display=function() {
+            document.write("<br> Employee object function")
+            document.write("<br> Id is "+this.id);
+            document.write("<br> Name is "+this.name);
+            document.write("<br> Age is "+this.age);
+        }
+}
+var emp1 = new Employee();
+emp1.display();*/
+//ES6 style 
+class Employee {
+    id=100;
+    name = "Ajay";
+    age = 21;
+    display() {
+        document.write("<br> Employee object function")
+        document.write("<br> Id is "+this.id);
+        document.write("<br> Name is "+this.name);
+        document.write("<br> Age is "+this.age);
+    }
+}
+let emp1 = new Employee();
+emp1.display();
+
--- Phase 4/JavaScript Using ES6/functionStyleObjectCreation.js
+++ Phase 4/JavaScript Using ES6/functionStyleObjectCreation.js
@@ -1,10 +1,35 @@
-// funtion style object creation 
-function Employee() {
-    this.id = 100;      // instance variable consider 
-    //var id = 100; // local variable consider 
-    this.name = "Ajay";
-    this.age = 21;
+// // funtion style object creation 
+// function Employee() {
+//     this.id = 100;      // instance variable consider 
+//     //var id = 100; // local variable consider 
+//     this.name = "Ajay";
+//     this.age = 21;
 
+//     this.display=function() {
+//         document.write("<br> Employee object function")
+//         document.write("<br> Id is "+this.id);
+//         document.write("<br> Name is "+this.name);
+//         document.write("<br> Age is "+this.age);
+//     }
+// }
+// //Employee();
+// var emp1 = new Employee();
+// var emp2 = new Employee();
+// document.write("<br> Id is "+emp1.id);
+// document.write("<br> Name is "+emp1.name);
+// document.write("<br> Age is "+emp1.age);
+// document.write("<br> Id is "+emp2.id);
+// document.write("<br> Name is "+emp2.name);
+// document.write("<br> Age is "+emp2.age);
+
+// emp1.display();
+// emp2.display();
+
+// Employee object with parameter 
+function Employee(id,name,age) {
+    this.id = id
+    this.name = name;
+    this.age = age;
     this.display=function() {
         document.write("<br> Employee object function")
         document.write("<br> Id is "+this.id);
@@ -13,8 +38,14 @@ function Employee() {
     }
 }
 //Employee();
-var emp1 = new Employee();
-document.write("<br> Id is "+emp1.id);
-document.write("<br> Name is "+emp1.name);
-document.write("<br> Age is "+emp1.age);
+    var emp1 = new Employee(101,"Steven",24);
+    var emp2 = new Employee(102,"Lex",28);
+// document.write("<br> Id is "+emp1.id);
+// document.write("<br> Name is "+emp1.name);
+// document.write("<br> Age is "+emp1.age);
+// document.write("<br> Id is "+emp2.id);
+// document.write("<br> Name is "+emp2.name);
+// document.write("<br> Age is "+emp2.age);
 emp1.display();
+emp2.display();
+
