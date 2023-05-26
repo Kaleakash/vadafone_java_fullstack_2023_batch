99102265cc98d8b2d4422cb536e83f695644bf00
Akash <akash300383@gmail.com>
2023-05-26 14:19:27 +0530

    done

---
+++ Phase 4/JavaScript Using ES6/AsynchDynamic.js
@@ -0,0 +1,16 @@
+function doTask() {
+    let n=0;
+    setInterval(()=> {
+        var dd = new Date();
+        if(n%2==0){
+            document.getElementsByTagName("h2")[0].style.color="RED";
+            document.getElementsByTagName("h2")[0].style.backgroundColor="YELLOW";
+        }else {
+            document.getElementsByTagName("h2")[0].style.color="GREEN";
+            document.getElementsByTagName("h2")[0].style.backgroundColor="BLUE";
+        }
+        document.getElementById("root").innerHTML=n  
+        document.getElementById("dateInfo").innerHTML=dd.getHours()+":"+dd.getMinutes()+":"+dd.getSeconds();
+        n++;
+    },1000)
+}
---
+++ Phase 4/JavaScript Using ES6/SynAndAsynDemo.js
@@ -0,0 +1,12 @@
+setTimeout(()=> {
+    document.write("<br/> 1st Statement");
+},5000)
+//document.write("1st Statement");
+//document.write("<br/>2nd Statement");
+setTimeout(()=> {
+    document.write("<br/> 2rd Statement");
+},3000)
+document.write("<br/>3rd Statement");
+setInterval(()=> {
+    document.write("<br/> 4th Statement");
+},1000)
