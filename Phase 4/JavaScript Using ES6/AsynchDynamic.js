088c6792ed1c251648b9844b9061696b97d9535c
Akash <akash300383@gmail.com>
2023-05-26 15:08:13 +0530

    done

--- Phase 4/JavaScript Using ES6/AsynchDynamic.js
+++ Phase 4/JavaScript Using ES6/AsynchDynamic.js
@@ -2,7 +2,7 @@ function doTask() {
     let n=0;
     setInterval(()=> {
         var dd = new Date();
-        if(n%2==0){
+        if(n%2==0){                     // 1/2, 3/2, 5/2 
             document.getElementsByTagName("h2")[0].style.color="RED";
             document.getElementsByTagName("h2")[0].style.backgroundColor="YELLOW";
         }else {
---
+++ Phase 4/JavaScript Using ES6/promiseDemo.js
@@ -0,0 +1,8 @@
+var obj = new Promise((resolved,rejected)=>{
+    //resolved("promise resolved")
+    rejected("promise rejected")
+})
+
+obj.then((data)=>document.write("<br/>Then "+data)).catch(error=>document.write("<br/>Error"+error))
+document.write("<br/>Hello")
+document.write("<br/>Hi")
