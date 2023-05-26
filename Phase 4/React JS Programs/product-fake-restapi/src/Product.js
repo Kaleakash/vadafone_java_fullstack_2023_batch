8adb034356300d682b1f4a1fa9206590e2ce4c87
Akash <akash300383@gmail.com>
2023-05-26 16:12:57 +0530

    done

--- Phase 4/React JS Programs/product-fake-restapi/src/Product.js
+++ Phase 4/React JS Programs/product-fake-restapi/src/Product.js
@@ -1,16 +1,25 @@
 import axios from "axios";
+import { useState } from "react";
 
 function Product() {
+let [products,setProduct]=useState([])
 let loadFakeData = function() {
     axios.get("https://api.escuelajs.co/api/v1/products").then(result=> {
-        console.log(result)
+        console.log(result.data)
+        setProduct(result.data);        // loaded data store in products
     }).catch(error=> {
         console.log(error)
     })
 }
+let product = products.map(p=><div>
+    <img src={p.images[0]} width="100px" height="100px"/>
+    {p.id}{p.title}{p.price}
+    </div>)
     return(
         <div>
             <input type="button" value="Load Data" onClick={loadFakeData}/>
+            <hr/>
+            {product}
         </div>
     )
 }
