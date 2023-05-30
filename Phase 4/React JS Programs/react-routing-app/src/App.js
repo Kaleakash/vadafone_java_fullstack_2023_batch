dabbd891af40b9b52775e9b29170666ef8963559
Akash <akash300383@gmail.com>
2023-05-30 16:29:28 +0530

    done

--- Phase 4/React JS Programs/react-routing-app/src/App.js
+++ Phase 4/React JS Programs/react-routing-app/src/App.js
@@ -2,6 +2,8 @@ import {Link, Route, Routes} from 'react-router-dom'
 import AboutUs from './AboutUs';
 import ContactUs from './ContactUs';
 import Feedback from './Feedback';
+import Login from './Login';
+import Home from './Home';
 function App() {
   return (
 
@@ -9,14 +11,16 @@ function App() {
      <h2>React Routing App</h2>  
       <Link to="/aboutus">About Us</Link> |
       <Link to="/contactus">Contact Us</Link> |
-      <Link to="/feedback">Feedback</Link>
-
+      <Link to="/feedback">Feedback</Link> |
+      <Link to="/login">Login</Link>
       <hr/>
 
       <Routes>
         <Route path='/aboutus' element={<AboutUs/>}></Route>
         <Route path='/contactus' element={<ContactUs/>}></Route>
         <Route path='/feedback' element={<Feedback/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
       </Routes>
     </div>
 
--- Phase 4/React JS Programs/react-routing-app/src/Login.js
+++ Phase 4/React JS Programs/react-routing-app/src/Login.js
@@ -1,8 +1,34 @@
-function Login() {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+function Login() {
+let [emailid,setEmailId]=useState("");
+let [password,setPassword]=useState("");
+let navigate = useNavigate();       // this hook help to navigate from one component to another component with condition. 
+let verifyUser = function(event){
+    event.preventDefault();
+    // we can pass username and password to backend technlogies using axios and 
+    // base upon result we can navigate to another component 
+    if(emailid=="akash@gmail.com" && password=="123"){
+            alert("successfully login")
+            navigate("/home");
+    }else {
+            alert("failure try once again")
+    }
+    setEmailId("");
+    setPassword("");
+}
     return(
         <div>
             <h2>Login Page</h2>
+            <form onSubmit={verifyUser}>
+                <label>EmailId</label>
+                <input type="email" name="emailid" onChange={(event)=>setEmailId(event.target.value)}/><br/>
+                <label>Password</label>
+                <input type="password" name="password" onChange={(event)=>setPassword(event.target.value)}/><br/>
+                <input type="submit" value="submit"/>
+                <input type="reset" value="reset"/>
+            </form>
         </div>
     )
 }
