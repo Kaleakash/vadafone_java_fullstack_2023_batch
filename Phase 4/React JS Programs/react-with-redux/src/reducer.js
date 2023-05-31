30d623a5293cdaa3df03db8b8795e5d35bb58138
Akash <akash300383@gmail.com>
2023-05-31 13:59:19 +0530

    done

--- Phase 4/React JS Programs/react-with-redux/src/reducer.js
+++ Phase 4/React JS Programs/react-with-redux/src/reducer.js
@@ -7,6 +7,14 @@ let initialState = {                // initialState hold one variable as counter
 function reducer(state = initialState,action){
         // base upon action we can do some changes on initialState with counter variable. 
        console.log(action);
+       switch(action.type){
+        case "INCREMENT":
+            //1st intial state, 2nd which property 
+            //3rd parameter value.
+            return {...state,counter:state.counter+1}
+        case "DECREMENT":
+            return {...state,counter:state.counter-1}
+       }
         return state;
 }
 
