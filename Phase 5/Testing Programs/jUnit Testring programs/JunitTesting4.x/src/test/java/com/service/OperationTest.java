dde2d718aacdfe06ec5da2ba8a88c469efd5d19a
Akash <akash300383@gmail.com>
2023-06-26 15:09:06 +0530

    done

--- Phase 5/Testing Programs/jUnit Testring programs/JunitTesting4.x/src/test/java/com/service/OperationTest.java
+++ Phase 5/Testing Programs/jUnit Testring programs/JunitTesting4.x/src/test/java/com/service/OperationTest.java
@@ -5,12 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class OperationTest {
-
-	@Test				// it is like a main method 
+	@Test
 	public void testAdd() {
 		Operation op = new Operation();
 		int result = op.add(100, 200);
-		assertEquals(30, result);
+		assertEquals(300, result);
+	}
+	@Test
+	public void testSub() {
+		Operation op = new Operation();
+		int result = op.sub(600, 200);
+		assertEquals(400, result);
+	}
+	@Test
+	public void testMul() {
+		Operation op = new Operation();
+		int result = op.mul(100, 2);
+		assertEquals(200, result);
+	}
+	@Test
+	public void testDiv() {
+		Operation op = new Operation();
+		int result = op.div(1000, 200);
+		assertEquals(5, result);
 	}
 
 }
