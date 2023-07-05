1da1050b3ab06ebe6c6c1bca81b5dd8b99520d65
Akash <akash300383@gmail.com>
2023-07-05 17:26:58 +0530

    done

---
+++ Phase 5/Docker Images/Creating Imge to run React JS Project/react-with-docker/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+       <h2>This project created by Akash Kale and goining to publish in docker hub</h2>
+        </p>
+        
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
